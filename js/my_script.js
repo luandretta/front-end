e26ad800d6ecff84157d79c0098d46f05b23e952
Lucimeri Andretta <luandretta@hotmail.com>
2023-02-14 09:31:19 +0000

    Feat: Add table.html and jquery

---
+++ js/my_script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+   /*  $("h2").css("text-decoration", "underline"); //underlines all <h2> elements
+    $("ul").css("border", "solid 1px #ccc"); //adds a border to all lists */
+    $("h2").addClass("underline");
+    $("ul").addClass("border");
+    // $("ul").removeClass("border");
+})
